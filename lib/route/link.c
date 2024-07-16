bisdn/libnl

commit bfee88b8b0a924404b15a88c734bf5a45d23d012

route: fix memory leak of l_info_ops in link_msg_parser()
[thaller@redhat.com: whitespace fix]

https://github.com/thom311/libnl/pull/254

lib/route/link.c
@@ -653,6 +653,9 @@ static int link_msg_parser(struct nl_cache_ops *ops, struct sockaddr_nl *who,
 					tb[IFLA_PROTINFO] = (struct nlattr *)link->l_af_ops->ao_protinfo_policy;
 			}
 
+			if (link->l_info_ops)
+				release_link_info(link);
+
 			ops = rtnl_link_info_ops_lookup(kind);
 			link->l_info_ops = ops;
 

commit c6e59f01e4cbc851cce2bc4778ab9bfc3cb7f669

link: keep link info instead of release and reacquire
Calling release_link_info() will call io_free() for rtnl_link::l_info
was allocated just a few lines before as a side effect of
rtnl_link_set_type().

Apart from doing needless memory churn, this will also leave the link
with l_info set to NULL if the link type does not provide a io_parse()
function.

Assuming the memory leak mentioned in bfee88b8b0a9 ("route: fix memory
leak of l_info_ops in link_msg_parser()") was a doubled reference
by calling rtnl_link_info_ops_lookup() twice, once via
rtnl_link_set_type(), and once directly.

So instead of releasing the link_info ops to fix it, just use the result
of the previous call and take the ops from link->l_info_ops.

Fixes: bfee88b8b0a9 ("route: fix memory leak of l_info_ops in link_msg_parser()")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

lib/route/link.c
@@ -681,11 +681,7 @@ static int link_msg_parser(struct nl_cache_ops *ops, struct sockaddr_nl *who,
 					tb[IFLA_PROTINFO] = (struct nlattr *)link->l_af_ops->ao_protinfo_policy;
 			}
 
-			if (link->l_info_ops)
-				release_link_info(link);
-
-			ops = rtnl_link_info_ops_lookup(kind);
-			link->l_info_ops = ops;
+			ops = link->l_info_ops;
 
 			if (ops) {
 				if (ops->io_parse &&