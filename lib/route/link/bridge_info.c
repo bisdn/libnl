bisdn/libnl

commit 7391a38e3d5edc5aac11c9d2fa0bf5ca67655bdb

bridge: Add support for link_info of a bridge
And functions to access some new bridge attributes.

Signed-off-by: Langer Thomas <tlanger@maxlinear.com>

Co-Authored-By: Kacper Ludwinski <kludwinski@maxlinear.com>

lib/route/link/bridge_info.c
@@ -1,0 +1,119 @@
+/*
+ * lib/route/link/bridge_info.c	bridge info support
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2022 MaxLinear, Inc.
+ */
+
+/**
+ * @ingroup link
+ * @defgroup bridge Bridging
+ *
+ * @details
+ * @{
+ */
+
+#include <netlink-private/netlink.h>
+#include <netlink-private/route/link/api.h>
+
+struct bridge_info
+{
+	uint32_t		ce_mask; /* to support attr macros */
+};
+
+static struct nla_policy bi_attrs_policy[IFLA_BR_MAX+1] = {
+
+};
+
+static inline struct bridge_info *bridge_info(struct rtnl_link *link)
+{
+	return link->l_info;
+}
+
+static int bridge_info_alloc(struct rtnl_link *link)
+{
+	struct bridge_info *bi;
+
+	if (link->l_info)
+		memset(link->l_info, 0, sizeof(*bi));
+	else {
+		bi = calloc(1, sizeof(*bi));
+		if (!bi)
+			return -NLE_NOMEM;
+
+		link->l_info = bi;
+	}
+
+	return 0;
+}
+
+static int bridge_info_parse(struct rtnl_link *link, struct nlattr *data,
+			     struct nlattr *xstats)
+{
+	struct nlattr *tb[IFLA_BR_MAX+1];
+	struct bridge_info *bi;
+	int err = 0;
+
+	NL_DBG(3, "Parsing Bridge link info\n");
+
+	if ((err = nla_parse_nested(tb, IFLA_BR_MAX, data, bi_attrs_policy)) < 0)
+		goto errout;
+
+	if ((err = bridge_info_alloc(link)) < 0)
+		goto errout;
+
+	bi = link->l_info;
+
+errout:
+	return err;
+}
+
+static int bridge_info_put_attrs(struct nl_msg *msg, struct rtnl_link *link)
+{
+	struct bridge_info *bi = link->l_info;
+	struct nlattr *data;
+
+	data = nla_nest_start(msg, IFLA_INFO_DATA);
+	if (!data)
+		return -NLE_MSGSIZE;
+
+	nla_nest_end(msg, data);
+	return 0;
+}
+
+static void bridge_info_free(struct rtnl_link *link)
+{
+	free(link->l_info);
+	link->l_info = NULL;
+}
+
+static struct rtnl_link_info_ops bridge_info_ops = {
+	.io_name		= "bridge",
+	.io_alloc		= bridge_info_alloc,
+	.io_parse		= bridge_info_parse,
+	.io_put_attrs		= bridge_info_put_attrs,
+	.io_free		= bridge_info_free,
+};
+
+#define IS_BRIDGE_INFO_ASSERT(link) \
+	do { \
+		if ((link)->l_info_ops != &bridge_info_ops) { \
+			APPBUG("Link is not a bridge link. Set type \"bridge\" first."); \
+		} \
+	} while(0)
+
+static void __init bridge_info_init(void)
+{
+	rtnl_link_register_info(&bridge_info_ops);
+}
+
+static void __exit bridge_info_exit(void)
+{
+	rtnl_link_unregister_info(&bridge_info_ops);
+}
+
+/** @} */

Makefile.am
@@ -404,6 +404,7 @@ lib_libnl_route_3_la_SOURCES = \
 	lib/route/link.c \
 	lib/route/link/api.c \
 	lib/route/link/bonding.c \
+	lib/route/link/bridge_info.c \
 	lib/route/link/bridge.c \
 	lib/route/link/can.c \
 	lib/route/link/dummy.c \

commit 4fc92b1f65b92e674a5b254e13c2ff4bbb78b2dd

bridge_info: implement missing rtnl_link_info_ops::io_clone()
Link info ops that use link info also need to implement
rtnl_link_info_ops::io_clone(), else cloning the link will result in a
broken object, and trying to use any rtnl_link_bridge_*() accessors will
result in a null pointer access due to rtnl_link::l_info not being taken
over.

So implement the missing rtnl_link_info_ops::io_clone() callback.

Fixes: 7391a38e3d5e ("bridge: Add support for link_info of a bridge")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

lib/route/link/bridge_info.c
@@ -72,6 +72,25 @@ static int bridge_info_alloc(struct rtnl_link *link)
 	return 0;
 }
 
+static int bridge_info_clone(struct rtnl_link *dst, struct rtnl_link *src)
+{
+	struct bridge_info *vdst, *vsrc = src->l_info;
+	int err;
+
+	err = bridge_info_alloc(dst);
+	if (err)
+		return err;
+
+	vdst = dst->l_info;
+
+	if (!vdst || !vsrc)
+		return -NLE_NOMEM;
+
+	memcpy(vdst, vsrc, sizeof(struct bridge_info));
+
+	return 0;
+}
+
 static int bridge_info_parse(struct rtnl_link *link, struct nlattr *data,
 			     struct nlattr *xstats)
 {
@@ -194,6 +213,7 @@ static void bridge_info_free(struct rtnl_link *link)
 static struct rtnl_link_info_ops bridge_info_ops = {
 	.io_name = "bridge",
 	.io_alloc = bridge_info_alloc,
+	.io_clone = bridge_info_clone,
 	.io_parse = bridge_info_parse,
 	.io_put_attrs = bridge_info_put_attrs,
 	.io_free = bridge_info_free,